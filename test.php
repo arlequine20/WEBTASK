4f70227fc92ca47cabeafe4d8d9957e031bd7d53
arlequine20 <maganzaarlequine250@gmail.com>
2026-03-02 23:31:00 +0200

    usingPhP

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        $name = "Alrequine Maganza";
+        echo "<h1>Hello World</h1>";
+        echo "<h2>My Name is $name </h2>"
+  ?>
+    </body>
+</html>
