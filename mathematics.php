f4a44b374f4f797fa016cb45c77ff6ae887a61d9
Arlequine <maganzaarlequine250@gmail.com>
2026-03-04 18:33:50 +0200

    Add files via upload

---
+++ mathematics.php
@@ -0,0 +1,20 @@
+<?php
+$first_number=5.99;
+$second_number=10.55;
+
+$ceil1=ceil($first_number);
+$ceil2=ceil($second_number);
+
+$sqrt1=sqrt($first_number);
+$sqrt2=sqrt($second_number);
+
+echo "Full numbers:$first_number and $second_number <br><br>";
+
+echo "Using ceil():<br>";
+echo "ceil($first_number) = $ceil1 <br>";
+echo "ceil($second_number) = $ceil2 <br><br>";
+
+echo "Using sqrt():<br>";
+echo "sqrt($first_number) = $sqrt1 <br>";
+echo "sqrt($second_number) = $sqrt2 <br>";
+?>
